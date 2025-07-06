62f981864a3fbbc3d8a335bd23a5a59f6e7ee4a0
Lucas Kent <rubickent@gmail.com>
2025-07-06 14:51:21 +1000

    add assertion to test

--- tests/tests/test.rs
+++ tests/tests/test.rs
@@ -1,12 +1,16 @@
 use authenticator::{
-    StatusPinUv, StatusUpdate,
+    Assertion, GetAssertionResult, StatusPinUv, StatusUpdate,
     authenticatorservice::{AuthenticatorService, SignArgs},
-    ctap2::server::{
-        AuthenticationExtensionsClientInputs, PublicKeyCredentialDescriptor, PublicKeyCredentialId,
-        Transport, UserVerificationRequirement,
+    ctap2::{
+        attestation::{AuthenticatorData, AuthenticatorDataFlags, Extension},
+        server::{
+            AuthenticationExtensionsClientInputs, AuthenticatorAttachment,
+            PublicKeyCredentialDescriptor, RpIdHash, Transport, UserVerificationRequirement,
+        },
     },
     statecallback::StateCallback,
 };
+use pretty_assertions::assert_eq;
 use std::sync::mpsc::{RecvError, channel};
 use std::thread;
 
@@ -82,7 +86,9 @@ fn test() {
         origin: "".to_owned(),
         relying_party_id: "".to_owned(),
         allow_list: vec![PublicKeyCredentialDescriptor {
-            id: PublicKeyCredentialId::default(),
+            id: "abcdefghijklmnopqrstuvwxyzabcdefghijklmnop"
+                .as_bytes()
+                .to_vec(),
             transports: vec![Transport::USB],
         }],
         user_verification_req: UserVerificationRequirement::Discouraged,
@@ -92,20 +98,52 @@ fn test() {
         use_ctap1_fallback: true,
     };
 
-    if let Err(e) = manager.sign(1000, sign_args, status_tx.clone(), callback) {
-        panic!("Couldn't register: {e:?}");
+    if let Err(e) = manager.sign(1000, sign_args, status_tx, callback) {
+        panic!("Couldn't sign: {e:?}");
     };
 
     let sign_result = sign_rx
         .recv()
         .expect("Problem receiving, unable to continue");
     let attestation_object = match sign_result {
-        Ok(a) => {
-            println!("Ok!");
-            a
-        }
+        Ok(a) => a,
         Err(e) => panic!("Registration failed: {e:?}"),
     };
-
-    println!("Register result: {:?}", &attestation_object);
+    assert_eq!(
+        GetAssertionResult {
+            assertion: Assertion {
+                credentials: Some(PublicKeyCredentialDescriptor {
+                    id: vec![
+                        // abcdef...
+                        97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112,
+                        113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 97, 98, 99, 100, 101,
+                        102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112
+                    ],
+                    transports: vec!(Transport::USB)
+                }),
+                auth_data: AuthenticatorData {
+                    rp_id_hash: RpIdHash([
+                        0xe3, 0xb0, 0xc4, 0x42, 0x98, 0xfc, 0x1c, 0x14, 0x9a, 0xfb, 0xf4, 0xc8,
+                        0x99, 0x6f, 0xb9, 0x24, 0x27, 0xae, 0x41, 0xe4, 0x64, 0x9b, 0x93, 0x4c,
+                        0xa4, 0x95, 0x99, 0x1b, 0x78, 0x52, 0xb8, 0x55
+                    ]),
+                    flags: AuthenticatorDataFlags::USER_PRESENT,
+                    counter: 0,
+                    credential_data: None,
+                    extensions: Extension::default(),
+                },
+                signature: vec![
+                    // rot13 of abcdef... is stored in an ASN.1 signature, split across two integers
+                    48, 68, 2, 32, 127, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121,
+                    122, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111,
+                    112, 113, 114, 2, 32, 127, 115, 116, 117, 118, 119, 120, 121, 122, 97, 98, 99,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                ],
+                user: None,
+            },
+            attachment: AuthenticatorAttachment::Unknown,
+            extensions: Default::default(),
+        },
+        attestation_object,
+    );
 }
